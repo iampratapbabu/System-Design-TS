26b61779233f4f7d39e1129a1697aad2adfd8112
Tej Pratap <bankebihari5463@gmail.com>
2025-09-27 23:59:35 +0530

    added interface

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 console.log("Hello, World!");
+
+interface IGreeter {
+    greet(name: string): string;
+}
+
+class Greeter implements IGreeter {
+    greet(name: string): string {
+        return `Hello, ${name}!`;
+    }
+}
+
+const greeter = new Greeter();
+console.log(greeter.greet("World"));
